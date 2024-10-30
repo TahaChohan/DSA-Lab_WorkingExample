daf8bc6c1dadf3562739ec80e358a72b09bf6ebf
Muhammad Taha <77847219+TahaChohan@users.noreply.github.com>
2024-10-30 13:45:11 +0500

    Add files via upload

---
+++ Searching_inArray.java
@@ -0,0 +1,65 @@
+class task1{
+
+    //Function to implment
+    //searchoperation 
+    static int findElement (int arry[], int n , int key){
+        for(int i = 0;i<n;i++)
+        {
+                if(arry[i] == key)
+                return i;
+        }
+        //if key not found 
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arry[] ={12,34,10,6,40};
+        int n = arry.length;
+
+        //using a last element as search element 
+        int key = 40;
+
+        //function call 
+
+        int position = findElement(arry, n, key);
+
+        if (position == -1 ) {
+            System.out.println("Element not found");
+
+        }
+        else{
+            System.out.println("Element found at position: " + (position+1));
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ insertion_end.java
@@ -0,0 +1,52 @@
+//insertion at end 
+public class task2 {
+    //Function to insert a given key in the array .This Function 
+    //Returns n+1 if insertion is successful, else n.
+
+    static int insertEnd(int array[], int n , int key , int capacity){
+        //Cannot insett more element if n is already more then the equal to capacity
+
+        if(n>capacity){
+            return n ;
+        }
+        array[n] = key ;
+        return (n+1);
+    }
+
+    public static void main(String[] args) {
+        int [] array = new int [20];
+        array[0] = 12 ;
+        array[1] = 16 ;
+        array[2] = 20 ;
+        array[3] = 40 ;
+        array[4] = 50 ;
+        array[5] = 70 ;
+        int capacity = 20 ;
+        int n = 6; 
+        int i , key = 26;
+
+        System.out.println("Before insertion: ");
+        for(i = 0 ; i < n ;i++){
+            System.out.println(array [i] + " ");
+        }
+
+        //inserting key 
+        n = insertEnd(array, n, key, capacity);
+        System.out.println("\n After Insertion");
+
+        for(i = 0 ; i < n ; i++){
+            System.out.println(array[i] + " ");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    
+}
